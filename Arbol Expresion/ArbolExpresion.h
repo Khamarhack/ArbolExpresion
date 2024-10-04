8ba57ab1f21656a9b385dd908191274359ee4d40
Khamarhack <143459006+Khamarhack@users.noreply.github.com>
2024-10-04 15:44:34 -0500

    Update ArbolExpresion.h

--- Arbol Expresion/ArbolExpresion.h
+++ Arbol Expresion/ArbolExpresion.h
@@ -1,3 +1,9 @@
+/******************************************
+Fecha: 1 octubre 2024
+Autor: Santiago Camargo
+Tema: Taller Arboles
+Estructuras de datos
+******************************************/
 #ifndef ARBOLEXPRESION_H
 #define  ARBOLEXPRESION_H
 #include "NodoExpresion.h"
