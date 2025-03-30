e0c7859d6fbf58e06fb8beca46b6233dcc5c8d35
Khamarhack <143459006+Khamarhack@users.noreply.github.com>
2025-03-30 11:48:25 -0500

    Update ArbolExpresion.hxx

--- Arbol Expresion/ArbolExpresion.hxx
+++ Arbol Expresion/ArbolExpresion.hxx
@@ -1,170 +1,61 @@
-#include "ArbolExpresion.h"
-#include <iostream>
+/*******************************************
+Fecha: 26 marzo 2025
+Autor: Santiago Camargo
+Tema: Taller Arboles
+Estructuras de datos
+******************************************/
+#ifndef ARBOLEXPRESION_H
+#define ARBOLEXPRESION_H
+
+#include "NodoExpresion.h"
 #include <string>
-#include <stack>
+
 using namespace std;
 
+/**
+ * Clase que representa un árbol de expresión matemática.
+ * Permite construir el árbol desde expresiones prefijas o posfijas,
+ * evaluarlo y obtener sus representaciones en distintas notaciones.
+ */
+class ArbolExpresion {
+protected:
+    NodoExpresion* raiz; // Raíz del árbol
 
-ArbolExpresion::ArbolExpresion(){
-	this->raiz=NULL;
-}
-ArbolExpresion::~ArbolExpresion(){
-}
-NodoExpresion* ArbolExpresion::getRaiz(){
-	return this->raiz;
-}
-void ArbolExpresion::setRaiz(NodoExpresion* nod){
-	this->raiz=nod;
-	return;
-}
-int ArbolExpresion::evaluar(NodoExpresion* nodi){
-	if(nodi->getHijoIzq()==NULL&&nodi->getHijoDer()==NULL){
+public:
+    // Constructor: inicializa el árbol vacío
+    ArbolExpresion();
+    
+    // Destructor: libera la memoria del árbol
+    ~ArbolExpresion();
 
-		return (int)(nodi->getDato()-48);
-	}else{
-		int resultado=0;
-		int izquierda=evaluar(nodi->getHijoIzq());
+    // Retorna la raíz del árbol
+    NodoExpresion* getRaiz();
 
-		int derecha=evaluar(nodi->getHijoDer());
-		char operando=nodi->getDato();
-		
-		switch (operando) {
-			case '+':
-			resultado=izquierda+derecha;
-			break;
-			case '-':
-			resultado=izquierda-derecha;
-			break;
-			case '*':
-			resultado=izquierda*derecha;
-			break;
-			case '/':
-			resultado=izquierda/derecha;
-			break;
-			default:
-			resultado=izquierda+derecha;
-			break;
-		}
-		return resultado;
-	}
+    // Establece la raíz del árbol
+    void setRaiz(NodoExpresion* nod);
 
-}
+    // Construye el árbol desde una expresión en notación prefija
+    void llenarDesdePrefija(string &expresion);
 
-void ArbolExpresion::llenarDesdePrefija(string &expresion){
+    // Construye el árbol desde una expresión en notación posfija
+    void llenarDesdePosfija(string &expresion);
 
-	char cad[expresion.size()];
-	strcpy(cad,expresion.c_str());
+    // Muestra la expresión en notación prefija
+    void obtenerPrefija(NodoExpresion* inicio);
 
-	stack<NodoExpresion* > miCola;
-	NodoExpresion* n1;
-	NodoExpresion* n2;
-	NodoExpresion* n3;
+    // Muestra la expresión en notación infija
+    void obtenerInfija(NodoExpresion* inicio);
 
-	for(int i=strlen(cad)-1;i>=0;i--){
+    // Muestra la expresión en notación posfija
+    void obtenerPosfija(NodoExpresion* inicio);
 
-		if(!siOperando(cad[i])){
+    // Evalúa el valor de la expresión en el árbol
+    int evaluar(NodoExpresion* nodi);
 
-			n1=new NodoExpresion();
-			n1->setDato(cad[i]);
-			miCola.push(n1);
-		}else{
-			n1=new NodoExpresion();
-			n1->setDato(cad[i]);
+    // Verifica si un carácter es un operando (número o variable)
+    bool siOperando(char car);
+};
 
-			n2=new NodoExpresion();
-			n2=miCola.top();
-			miCola.pop();
-			n3=new NodoExpresion();
-			n3=miCola.top();
-			miCola.pop();
+#include "ArbolExpresion.hxx"
 
-			n1->setHijoDer(n2);
-			n1->setHijoIzq(n3);
-
-			miCola.push(n1);
-		}
-	}
-	n1=miCola.top();
-	this->setRaiz(n1);
-	miCola.pop();
-}
-
-void ArbolExpresion::llenarDesdePosfija(string &expresion){
-
-	char cad[expresion.size()];
-	strcpy(cad,expresion.c_str());
-
-	stack<NodoExpresion* > miCola;
-
-	NodoExpresion* n1;
-	NodoExpresion* n2;
-	NodoExpresion* n3;
-
-	for(int i=0;i<strlen(cad);i++){
-
-		if(!siOperando(cad[i])){
-
-			n1=new NodoExpresion();
-			n1->setDato(cad[i]);
-			miCola.push(n1);
-		}else{
-			n1=new NodoExpresion();
-			n1->setDato(cad[i]);
-
-			n2=new NodoExpresion();
-			n2=miCola.top();
-			miCola.pop();
-			n3=new NodoExpresion();
-			n3=miCola.top();
-			miCola.pop();
-
-			n1->setHijoDer(n2);
-			n1->setHijoIzq(n3);
-
-			miCola.push(n1);
-		}
-	}
-	n1=miCola.top();
-	this->setRaiz(n1);
-	miCola.pop();
-}
-
-void ArbolExpresion::obtenerPrefija(NodoExpresion* inicio){
-	if(inicio==NULL){
-		return;
-	}else{
-		cout<<inicio->getDato()<<" ";
-		this->obtenerPrefija(inicio->getHijoIzq());
-		this->obtenerPrefija(inicio->getHijoDer());
-	}
-}
-void ArbolExpresion::obtenerInfija(NodoExpresion* inicio){
-	if(this->raiz!=NULL){
-		if(inicio->getHijoIzq()!=NULL){
-			obtenerInfija(inicio->getHijoIzq());
-		}
-		cout << inicio->getDato()<<" ";
-		if(inicio->getHijoDer()!=NULL){
-			obtenerInfija(inicio->getHijoDer());
-		}
-	}else{
-		cout << "El arbol esta vacio" << '\n';
-	}
-}
-void ArbolExpresion::obtenerPosfija(NodoExpresion* inicio){
-	if(inicio==NULL){
-		return;
-	}else{
-		this->obtenerPosfija(inicio->getHijoIzq());
-		this->obtenerPosfija(inicio->getHijoDer());
-		cout<<inicio->getDato()<<" ";
-	}
-}
-
-bool ArbolExpresion::siOperando(char car){
-	if(car=='+'||car=='-'||car=='/'||car=='*'||car=='%'){
-		return true;
-	}else{
-		return false;
-	}
-}
+#endif // ARBOLEXPRESION_H
