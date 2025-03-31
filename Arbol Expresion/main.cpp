359c624e9b252a630e4f34d8c779fc2aa146019d
Khamarhack <143459006+Khamarhack@users.noreply.github.com>
2025-03-30 21:27:54 -0500

    Update main.cpp

--- Arbol Expresion/main.cpp
+++ Arbol Expresion/main.cpp
@@ -1,3 +1,9 @@
+/*******************************************
+Fecha: 26 marzo 2025
+Autor: Santiago Camargo
+Tema: Taller Arboles
+Estructuras de datos
+******************************************/
 #include<iostream>
 #include "ArbolExpresion.h"
 #include "NodoExpresion.h"
